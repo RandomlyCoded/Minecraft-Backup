9d2d4e8e73c41ab25b63d1f30065a94aa432ed2c
RandomlyCoded <marc+github@taschenorakel.de>
2023-12-27 09:21:32 +0100

    rewriting to move into a tar archive instead of a new directory

--- backup.cpp
+++ backup.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
+void sysWrap(const char *cmd)
+{
+#ifdef DEBUG_BUILD
+    cout << "\033[01;33m" << cmd << "\033[0m\b" << endl;
+#endif // DEBUG_BUILD
+
+    system(cmd);
+}
+
 void backupDir (string world, filesystem::path dir)
 {
-    cout << "backing up \033[01;34m" << (dir.empty () ? "world root" : dir.relative_path ().c_str ()) << "\033[00m" << endl;
+    cout << "backup \033[01;34m" << (dir.empty () ? "world root" : dir.relative_path ().c_str ()) << "\033[00m" << endl;
 
-    // create directory
-    system (("mkdir " + world + "-BACKUP/" + dir.relative_path ().string () + "/ 2> /dev/null").c_str ());
-
-    // copy content
-    system (("cp " + world + "/" + dir.relative_path ().string () + "/* " + world + "-BACKUP/" + dir.relative_path ().string () + "/ 2> /dev/null").c_str ());
+    // add directory to tar file
+    sysWrap (("tar --no-recursion -" + ((dir.empty () ? string("c") : string("r")) + "f ../backups/" + world) + "-backup.tar.xz " + world + "/" + dir.string () + "/*").c_str ());
 }
 
 void backupSave (string world)
 {
-    cout << "starting to back up \033[01;35m" << world << "\033[00m" << endl;
+    cout << "starting to backup \033[01;35m" << world << "\033[00m" << endl;
 
-    // we need to replace every whitespace with "\ " for cp to work properly
+    // we need to replace every whitespace with "\ "
     string worldFixed;
 
     for (auto c: world) {
@@ -27,22 +33,17 @@ void backupSave (string world)
         worldFixed += c;
     }
 
-    world = worldFixed;
-
     // clear previous backup
-    system (("rm " + world + "-BACKUP/ -r 2> /dev/null").c_str ());
-
-    // create backup
-    system (("mkdir " + world + "-BACKUP/").c_str ());
+    sysWrap (("rm ../backups/" + world + "-backup.tar.xz -r").c_str ());
 
     // backup files in the world root
-    backupDir (world, "");
+    backupDir (worldFixed, "");
 
     // backup all the directories from the main world
     for (auto& p : filesystem::recursive_directory_iterator (world))
         if (p.is_directory ()) {
             auto path = filesystem::path (p.path().c_str () + world.length () + 1); // remove "world/" from beginning
-            backupDir (world, path);
+            backupDir (worldFixed, path);
         }
 }
 
@@ -55,7 +56,7 @@ int main ()
 
     if (!filesystem::exists(world)) {
         cout << "\033[01;04;31mworld does not exist, here are all available ones\033[00m" << endl;
-        system ("ls");
+        sysWrap ("ls");
 
         return 1;
     }
