04db44bf24a1e592f1c8759f237f96c11ab49397
RandomlyCoded <marc+github@taschenorakel.de>
2024-07-20 11:28:04 +0200

    change the GUI program into an actual QML application
    now the only thing left to do is actually implementing the QML code...

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,76 +1,52 @@
 #include "backup.h"
 #include "commandLine.h"
-#include "utils.h"
 
 #include "qmlguihandler.h"
 
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
 using namespace std;
 
-using namespace randomly;
-
-// Helper for underlining the first letter of a text
-struct FirstLetterUnderlined
+namespace randomly
 {
-    const char *txt;
 
-private:
-    friend std::ostream& operator<<(std::ostream& os, FirstLetterUnderlined text)
-    {
-        os << (Green | Bold | Underline) << text.txt[0];
+class Application : public QGuiApplication
+{
+public:
+    using QGuiApplication::QGuiApplication;
 
-        os << (Green | Bold | UnderlineOff) << (text.txt + 1);
-
-        return os;
-    }
+    // I don't like passing argc and argv during construction AND calling, but that's the way I implemented the command line parsing...
+    int run(int argc, char **argv, QUrl qmlRoot = {});
 };
 
+int Application::run(int argc, char **argv, QUrl qmlRoot)
+{
+    if (qmlRoot.isEmpty())
+        return run(argc, argv, {"qrc:/main.qml"});
+
+    auto options = parseCommandLine(argc, argv);
+    options.useGUI = true;
+
+    auto gui = new QmlGuiHandler{options};
+
+    Backup backup(options, gui);
+
+    QQmlApplicationEngine qml;
+
+    qmlRegisterSingletonInstance<QmlGuiHandler>("McBackupGUI", 1, 0, "GuiHandler", gui);
+
+    qml.load(qmlRoot);
+
+    if(qml.rootObjects().isEmpty())
+        return EXIT_FAILURE;
+
+    return exec();
+}
+
+}
 
 int main(int argc, char **argv)
 {
-    auto options = parseCommandLine(argc, argv);
-
-    if (!options.backupSet) {
-        cout << (Green | Bold)
-             << FirstLetterUnderlined{"Backup or "} << FirstLetterUnderlined{"Restore world? (B / r)"} << endl
-             << Default;
-
-        string response;
-        getline(cin, response);
-
-        if (response.empty() || response[0] == 'B' || response[0] == 'b')
-            options.backup = true;
-
-        else if(response[0] == 'r' || response[0] == 'R')
-            options.backup = false;
-
-        else {
-            cerr << (Red | Underline)
-                 << "invalid response." << endl
-                 << Default;
-
-            return EXIT_FAILURE;
-        }
-    }
-
-    if (options.world.empty()) {
-        cout << (Green | Bold)
-             << "enter save name" << endl
-             << Default;
-
-        getline (cin, options.world); // getline reads a whole line with whitespaces
-    }
-    else
-        cout << "using world from command line" << endl;
-
-    options.useGUI = true;
-
-    Backup backup(options, new QmlGuiHandler{options});
-
-    if(options.backup)
-        backup.backupSave(options.world);
-
-    else
-        backup.restoreSave(options.world);
-
-    return 0;
+    return randomly::Application{argc, argv}.run(argc, argv);
 }
