62d52afbbe562453651c7bc8ddd34b863b945e05
RandomlyCoded <marc+github@taschenorakel.de>
2024-07-20 09:05:27 +0200

    split project into a core library and separate executable for GUI and Terminal UI

---
+++ src/gui-main.cpp
@@ -0,0 +1,74 @@
+#include "commandLine.h"
+#include "backup.h"
+#include "utils.h"
+
+using namespace std;
+
+using namespace randomly;
+
+// Helper for underlining first letter
+struct FirstLetterUnderlined
+{
+    const char *txt;
+
+private:
+    friend std::ostream& operator<<(std::ostream& os, FirstLetterUnderlined text)
+    {
+        os << (Green | Bold | Underline) << text.txt[0];
+
+        os << (Green | Bold | UnderlineOff) << (text.txt + 1);
+
+        return os;
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+    auto options = parseCommandLine(argc, argv);
+
+    if (!options.backupSet) {
+        cout << (Green | Bold)
+             << FirstLetterUnderlined{"Backup or "} << FirstLetterUnderlined{"Restore world? (B / r)"} << endl
+             << Default;
+
+        string response;
+        getline(cin, response);
+
+        if (response.empty() || response[0] == 'B' || response[0] == 'b')
+            options.backup = true;
+
+        else if(response[0] == 'r' || response[0] == 'R')
+            options.backup = false;
+
+        else {
+            cerr << (Red | Underline)
+                 << "invalid response." << endl
+                 << Default;
+
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (options.world.empty()) {
+        cout << (Green | Bold)
+             << "enter save name" << endl
+             << Default;
+
+        getline (cin, options.world); // getline reads a whole line with whitespaces
+    }
+    else
+        cout << "using world from command line" << endl;
+
+    options.useGUI = true;
+
+    Backup backup(options);
+
+    if(options.backup)
+        backup.backupSave(options.world);
+
+    else
+        backup.restoreSave(options.world);
+
+    return 0;
+}
